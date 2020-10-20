aa969a34774f87fbde0436f01df28d53867b59f0
janbraunsdorff <jan99braunsdorff@web.de>
2020-10-20 09:06:38 +0200

    update Exception type for Crud Bank Repo

--- src/main/java/de/janbraunsdorff/ase/tech/repositories/CrudBankRepository.java
+++ src/main/java/de/janbraunsdorff/ase/tech/repositories/CrudBankRepository.java
@@ -1,12 +1,11 @@
 package de.janbraunsdorff.ase.tech.repositories;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface CrudBankRepository {
-    BankEntity get(String Id) throws IOException;
-    List<BankEntity> get() throws IOException;
+    BankEntity get(String Id) throws Exception;
+    List<BankEntity> get() throws Exception;
     BankEntity create(BankEntity bankEntity) throws Exception;
-    BankEntity update(BankEntity bankEntity) throws IOException;
-    boolean delete(String bankId) throws IOException;
+    BankEntity update(BankEntity bankEntity) throws Exception;
+    boolean delete(String bankId) throws Exception;
 }
