23e9a259d8e4168f856197d4a354e504da11344f
janbraunsdorff <jan99braunsdorff@web.de>
2020-10-22 12:35:57 +0200

    format bank help

--- src/main/java/de/janbraunsdorff/ase/userinterface/console/result/bank/BankHelpResult.java
+++ src/main/java/de/janbraunsdorff/ase/userinterface/console/result/bank/BankHelpResult.java
@@ -12,14 +12,15 @@ public class BankHelpResult implements Result {
     @Override
     public List<SentencePiece> print() {
         final SentencePiece newCommand = new Command();
+        final int commandSize = 50;
         return new ArrayList<SentencePiece>() {{
             add(new SentencePiece(Color.WHITE, "Hilfe (Bank):\n"));
             add(newCommand);
-            add(new SentencePiece(Color.WHITE, String.format("%-30s\t%s\n", "bank all", "zeigt alle Banken an")));
+            add(new SentencePiece(Color.WHITE, String.format("%-"+commandSize+"s\t%s\n", "bank all", "zeigt alle Banken an")));
             add(newCommand);
-            add(new SentencePiece(Color.WHITE, String.format("%-30s\t%s\n", "bank add -n [name]", "legt eine neue Bank mit einem Name an")));
+            add(new SentencePiece(Color.WHITE, String.format("%-"+commandSize+"s\t%s\n", "bank add -n [name] -a [abkürzung]", "legt eine neue Bank mit einem Name und einer Abkürzung an")));
             add(newCommand);
-            add(new SentencePiece(Color.WHITE, String.format("%-30s\t%s\n", "bank update -id [ID] -n [name]", "aktualiserit den Namen einer Bank")));
+            add(new SentencePiece(Color.WHITE, String.format("%-"+commandSize+"s\t%s\n", "bank update -id [ID] -n [name]", "aktualiserit den Namen einer Bank")));
             add(new SentencePiece(Color.BASE, "\n"));
         }};
     }
