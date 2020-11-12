d282f8bf0d98d2a01b36792a0400ba92fb1247bd
janbraunsdorff <jan99braunsdorff@web.de>
2020-11-12 21:06:59 +0100

    update transaction entity

--- src/main/java/de/janbraunsdorff/ase/layer/persistence/repositories/entität/TransactionEntity.java
+++ src/main/java/de/janbraunsdorff/ase/layer/persistence/repositories/entität/TransactionEntity.java
@@ -8,27 +8,24 @@ public class TransactionEntity {
     private String id;
     private Integer value;
     private Date date;
-    private String form;
-    private String to;
+    private String thirdParty;
     private String category;
     private Boolean isContract;
 
-    public TransactionEntity(Integer value, Date date, String form, String to, String category, Boolean isContract) {
+    public TransactionEntity(Integer value, Date date, String thirdParty, String category, Boolean isContract) {
         this.id = UUID.randomUUID().toString();
         this.value = value;
         this.date = date;
-        this.form = form;
-        this.to = to;
+        this.thirdParty = thirdParty;
         this.category = category;
         this.isContract = isContract;
     }
 
-    public TransactionEntity(Integer value, String form, String to, String category, Boolean isContract) {
+    public TransactionEntity(Integer value, String thirdParty, String category, Boolean isContract) {
         this.id = UUID.randomUUID().toString();
         this.date = Calendar.getInstance().getTime();
         this.value = value;
-        this.form = form;
-        this.to = to;
+        this.thirdParty = thirdParty;
         this.category = category;
         this.isContract = isContract;
     }
