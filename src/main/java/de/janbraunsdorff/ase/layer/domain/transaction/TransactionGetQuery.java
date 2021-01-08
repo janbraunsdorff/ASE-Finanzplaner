dcf6d0b8e9b5799e80ac3b01d414b45974c16fd4
janbraunsdorff <jan99braunsdorff@web.de>
2021-01-08 13:35:23 +0100

    reconstruct query

---
+++ src/main/java/de/janbraunsdorff/ase/layer/domain/transaction/TransactionGetQuery.java
@@ -0,0 +1,20 @@
+package de.janbraunsdorff.ase.layer.domain.transaction;
+
+
+public class TransactionGetQuery {
+    private final String account;
+    private final Integer count;
+
+    public TransactionGetQuery(String account, Integer count) {
+        this.account = account;
+        this.count = count;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+}
