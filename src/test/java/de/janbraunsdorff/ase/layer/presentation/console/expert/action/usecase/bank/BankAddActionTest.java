cbf7a261bf4ff3f40381d049df121fca155aca52
janbraunsdorff <jan99braunsdorff@web.de>
2021-02-16 17:44:34 +0100

    add test for bank add

--- src/test/java/de/janbraunsdorff/ase/layer/presentation/console/expert/action/usecase/bank/BankAddActionTest.java
+++ src/test/java/de/janbraunsdorff/ase/layer/presentation/console/expert/action/usecase/bank/BankAddActionTest.java
@@ -5,12 +5,16 @@ import de.janbraunsdorff.ase.layer.domain.AcronymAlreadyExistsException;
 import de.janbraunsdorff.ase.layer.presentation.console.expert.ExpertCommand;
 import de.janbraunsdorff.ase.layer.presentation.console.expert.action.Result;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 class BankAddActionTest {
 
+    @DisplayName("addBank")
     @Test
     public void addBank() throws AcronymAlreadyExistsException {
         var app = new BankApplicationTestImplementation();
@@ -26,5 +30,22 @@ class BankAddActionTest {
         assertThat(app.name, Matchers.is("name"));
         assertThat(app.acronym, Matchers.is("acronym"));
         assertThat(app.type, Matchers.is("type"));
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+            "bank add -n name -t types",
+            "bank add -a acronym -t type",
+            "bank add -a acronym -n name"
+    })
+    public void missingParameterAddBank(String commandString) throws AcronymAlreadyExistsException {
+        var app = new BankApplicationTestImplementation();
+        var action = new BankAddAction(app);
+        var command = new ExpertCommand(commandString, 2);
+
+        Result act = action.act(command);
+
+        // Check Return Type
+        assertThat(act, Matchers.instanceOf(BankHelpResult.class));
     }
 }
