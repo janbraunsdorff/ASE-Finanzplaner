b0991fc4be3a1b8e3c286fc984c616a9c471597b
janbraunsdorff <jan99braunsdorff@web.de>
2021-02-25 16:29:22 +0100

    test ListBank

---
+++ src/test/java/de/janbraunsdorff/ase/layer/presentation/console/directory/bank/ListBankTest.java
@@ -0,0 +1,25 @@
+package de.janbraunsdorff.ase.layer.presentation.console.directory.bank;
+
+import de.janbraunsdorff.ase.layer.presentation.console.directory.Hierarchy;
+import de.janbraunsdorff.ase.layer.presentation.console.directory.OverlayCommand;
+import de.janbraunsdorff.ase.layer.presentation.console.directory.State;
+import de.janbraunsdorff.ase.layer.presentation.console.directory.StateTransition;
+import de.janbraunsdorff.ase.layer.presentation.console.expert.ExpertCommand;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+class ListBankTest {
+
+    @Test
+    public void build(){
+        var state = new State(Hierarchy.BANK, null, null);
+        var cmd = new ExpertCommand("ls", 0);
+        var act = new ListBank();
+        OverlayCommand res = act.build(state, cmd);
+
+        assertThat(res.command().getInput(), Matchers.is("bank all"));
+        assertThat(res.transition(), Matchers.is(StateTransition.STAY));
+    }
+}
