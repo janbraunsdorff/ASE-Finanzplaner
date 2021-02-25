f0a5abb5e9dc75fc5178482b63d69a497e7bdba2
janbraunsdorff <jan99braunsdorff@web.de>
2021-02-25 16:10:57 +0100

    test bank actor

---
+++ src/test/java/de/janbraunsdorff/ase/layer/presentation/console/directory/bank/BankActorTest.java
@@ -0,0 +1,54 @@
+package de.janbraunsdorff.ase.layer.presentation.console.directory.bank;
+
+import de.janbraunsdorff.ase.layer.presentation.console.directory.*;
+import de.janbraunsdorff.ase.layer.presentation.console.expert.ExpertCommand;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+class BankActorTest {
+
+    @Test
+    public void act(){
+        var actor = new BankActor();
+        actor.addBuilder("trigger", new CalledCommandBuilder());
+
+        var command = new ExpertCommand("trigger", 0);
+        var state = new State(Hierarchy.BANK, null, null);
+
+        OverlayCommand act = actor.act(state, command);
+
+        assertThat(act.command().getInput(), Matchers.is("state: BANK command: trigger"));
+        assertThat(act.transition(), Matchers.is(StateTransition.STAY));
+
+    }
+
+    @Test
+    public void actDefault(){
+        var actor = new BankActor();
+
+        var command = new ExpertCommand("trigger", 0);
+        var state = new State(Hierarchy.BANK, null, null);
+
+        OverlayCommand act = actor.act(state, command);
+
+        assertThat(act.command().getInput(), Matchers.is("bank"));
+        assertThat(act.transition(), Matchers.is(StateTransition.STAY));
+
+    }
+
+
+    private static class CalledCommandBuilder implements CommandBuilder {
+        public State state;
+        public ExpertCommand command;
+
+        @Override
+        public OverlayCommand build(State state, ExpertCommand command) {
+            this.state = state;
+            this.command = command;
+            return new OverlayCommand( new ExpertCommand("state: " + state.getHierarchy() + " command: " + command.getInput(), 0), StateTransition.STAY);
+        }
+    }
+}
