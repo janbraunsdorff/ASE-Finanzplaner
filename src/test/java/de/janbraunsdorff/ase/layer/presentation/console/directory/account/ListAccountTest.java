fd41674468e7e4af244ff80cc9fbb8e7bed70e1b
janbraunsdorff <jan99braunsdorff@web.de>
2021-03-02 10:38:43 +0100

    test list account

---
+++ src/test/java/de/janbraunsdorff/ase/layer/presentation/console/directory/account/ListAccountTest.java
@@ -0,0 +1,26 @@
+package de.janbraunsdorff.ase.layer.presentation.console.directory.account;
+
+import de.janbraunsdorff.ase.layer.presentation.console.directory.Hierarchy;
+import de.janbraunsdorff.ase.layer.presentation.console.directory.OverlayCommand;
+import de.janbraunsdorff.ase.layer.presentation.console.directory.State;
+import de.janbraunsdorff.ase.layer.presentation.console.directory.StateTransition;
+import de.janbraunsdorff.ase.layer.presentation.console.expert.ExpertCommand;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListAccountTest {
+    @Test
+    public void build(){
+        var builder = new ListAccount();
+        State sate = new State(Hierarchy.ACCOUNT, "bank", null);
+        ExpertCommand command = new ExpertCommand("ls", 1);
+        OverlayCommand res = builder.build(sate, command);
+
+        assertThat(res.command().getInput(), Matchers.is("account all -a bank"));
+        assertThat(res.transition(), Matchers.is(StateTransition.STAY));
+
+    }
+}
