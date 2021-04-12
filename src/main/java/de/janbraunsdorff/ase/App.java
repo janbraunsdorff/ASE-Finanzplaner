adcdc73cbc1d4749658a43c113f35eac908d0b42
janbraunsdorff <jan99braunsdorff@web.de>
2021-04-12 13:56:13 +0200

    change decimal point

--- src/main/java/de/janbraunsdorff/ase/App.java
+++ src/main/java/de/janbraunsdorff/ase/App.java
@@ -2,10 +2,14 @@ package de.janbraunsdorff.ase;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 public class App {
 
     public static void main(String[] args) throws IOException, URISyntaxException {
+        NumberFormat nf = NumberFormat.getNumberInstance(Locale.GERMAN);
+
         new ApplicationWeb().run(args);
     }
 
